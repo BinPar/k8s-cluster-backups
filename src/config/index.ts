b8ba41dcaddda5c50ff74f1964450217adfd5669
drnachio <nacho@binpar.com>
2020-10-29 08:50:12 +0100

    General Improves

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,19 @@
+let maxHistoricalBackups = 5;
+
+if (process.env.MAX_HISTORICAL_BACKUPS) {
+  maxHistoricalBackups = parseInt(process.env.MAX_HISTORICAL_BACKUPS, 10);
+}
+
 /**
  * Global config
  */
 export default {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017',
-  bucketName: process.env.BUCKET_NAME || '',  
-  accessKeyId: process.env.ACCESS_KEY_ID || '',  
+  bucketName: process.env.BUCKET_NAME || '',
+  accessKeyId: process.env.ACCESS_KEY_ID || '',
   secretAccessKey: process.env.SECRET_ACCESS_KEY || '',
-  accountId:  process.env.ACCOUNT_ID || '',
-  mongoUser:  process.env.MONGO_USER || '',
-  mongoPassword:  process.env.MONGO_PASSWORD || '',
+  accountId: process.env.ACCOUNT_ID || '',
+  mongoUser: process.env.MONGO_USER || '',
+  mongoPassword: process.env.MONGO_PASSWORD || '',
+  maxHistoricalBackups
 };
