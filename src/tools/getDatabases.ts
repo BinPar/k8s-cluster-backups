832235f6c3c29df37b2495f70454292930c6195b
Ignacio Ferro Picón <nacho@binpar.com>
2021-03-02 22:41:42 +0100

    Improves and full

--- src/tools/getDatabases.ts
+++ src/tools/getDatabases.ts
@@ -3,6 +3,7 @@ import exceptions from '../data/exceptions.json';
 import weekly from '../data/weekly.json';
 import logger from '../services/logger';
 
+const forceFullBackup = process.env.FORCE_FULL_BACKUP === 'on';
 /**
  * Gets the list of databases in the current mongoDB instance
  */
@@ -13,11 +14,12 @@ const getDatabases = async (): Promise<string[]> => {
       (db: { name: string }): string => db.name,
     );
     const isSunday = new Date().getDay() === 0;
+    const isFull = isSunday || forceFullBackup;
     // We filter only:
     // Not Exceptions and
     // Weekly databases except in Sunday
     databaseNames = databaseNames.filter(
-      (name: string): boolean => exceptions.indexOf(name) === -1 && (isSunday || weekly.indexOf(name) === -1),
+      (name: string): boolean => exceptions.indexOf(name) === -1 && (isFull || weekly.indexOf(name) === -1),
     );
     return databaseNames;
   } catch (ex) {
